3b8e2f57eae2cf6aeb5b93c486ab3fbf9c1295fb
KallY-Py <jcporcopio03@gmail.com>
2026-05-26 20:53:59 +0800

    build for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/KJ12/',  // Replace KJ12 with your repo name
-  outputDir: 'docs'      // Build to docs folder instead of dist
+
+  publicPath: '/JK12/',
+  outputDir: 'docs',
+
+  productionSourceMap: false
 })
