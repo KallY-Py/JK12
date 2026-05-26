ecf6e4702b78bc90840ba3caa87dbc11639406c2
KallY-Py <jcporcopio03@gmail.com>
2026-05-26 20:27:19 +0800

    Add .nojekyll file

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  publicPath: '/KJ12/',
-
-  outputDir: 'docs'
+  publicPath: '/KJ12/',  // Replace KJ12 with your repo name
+  outputDir: 'docs'      // Build to docs folder instead of dist
 })
